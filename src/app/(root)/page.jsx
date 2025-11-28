f703d811fcf066aeac9c696d2b439ac65bc2aeac
Abrar Jahin Ayat <abrarjahinayat123@gmail.com>
2025-11-29 00:42:39 +0600

    category section added

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,12 +1,13 @@
 import Banner from '@/components/home/Banner'
-import Category from '@/components/home/category'
+import Category from '@/components/home/Cetagory'
+
 import React from 'react'
 
 const page = () => {
   return (
     <div>
         <Banner/>
-       <Category/>
+        <Category/>
     </div>
   )
 }
